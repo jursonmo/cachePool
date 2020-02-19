e2c35cba50f9e6cae9a92ac152930629d2b2ca50
myname <myname@example.com>
2020-02-19 12:28:45 +0800

    why stop ring_ver2

--- ring_ver2.go
+++ ring_ver2.go
@@ -49,6 +49,11 @@ func (r *ringEntryPosition2) PutEntry(eh *EntryHeader) bool {
 		   ，而 tail 是一个旧的值，所以 head-tail 会比实际的要大，即这时n==cap 的情况实际没满，需要try again
 
 		all: 当n>=cap时，无法确定是return 还是 try again
+
+		这里的应用跟disruptor 不一样，disruptor消费者 线程只要读不到就一直循环读，不会退出，生产者写入数据后，消费者
+		可以立即拿到数据并处理完，处理完后又一直循环读，即为了生产者的数据得到快速、低延迟的处理。
+		而这里的需求是内存池的需求，即内存池里对象可以用就一定要用上，而不能保证别的线程把内存对象放到这个池里，也就是当
+		发现内存池没有对象可用时，不能一直循环等内存池有对象为止。
 		*/
 		n := head - tail
 		if n >= cap {
